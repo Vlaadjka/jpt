5485a1f4875198913d6822067847851bc18decd5
Vlad <vladislavs.maksimcuks@gmail.com>
2017-12-09 10:48:27 +0200

    using hashmap instead of vector, added inc and set for jobs progress

--- src/jpt/job.clj
+++ src/jpt/job.clj
@@ -6,7 +6,7 @@
 
 (def job-expire-period-seconds 60)
 (def job-id (atom 1))
-(def job-db (atom []))
+(def job-db (atom {}))
 
 (defn- current-unix-timestamp
   "Current unix timestamp (seconds)"
@@ -14,33 +14,97 @@
   (quot (System/currentTimeMillis) 1000))
 
 (defn- unexpired-jobs
-  "Returns unexpired jobs which are created/updated less that expire-period-seconds ago"
+  "Returns jobs which are created/updated less that expire-period-seconds ago"
   [jobs expire-period-seconds]
-  (let [timestamp (current-unix-timestamp)]
-    (filter #(<= (- timestamp (get % :updated-at))
-                 expire-period-seconds)
-            jobs)))
+  (let [timestamp (current-unix-timestamp)
+        jobs (into {}
+                   (filter #(<= (- timestamp (:updated-at (val %)))
+                                expire-period-seconds)
+                           jobs))]
+    (if (empty? jobs)
+      {}
+      jobs)))
 
 (defn get-jobs
   "Removes all expired jobs from db and returns new list"
   []
-  (map
-   #(dissoc % :updated-at)
-   (swap! job-db unexpired-jobs job-expire-period-seconds)))
+  (swap! job-db unexpired-jobs job-expire-period-seconds))
 
 (defn add-new-job
   "Adds new job to db with 0 progress and returns new jobs map"
   [{total :total}]
-  (let [job {:id (swap! job-id inc)
+  (let [id (swap! job-id inc)
+        job {:id id
              :total total
              :progress 0
              :updated-at (current-unix-timestamp)}]
-    (swap! job-db conj job)
+    (swap! job-db assoc (keyword (str id)) job)
     job))
+
+(defn get-job
+  "Return single job by id"
+  [id]
+  ((keyword (str id)) (get-jobs)))
+
+(defn inc-progress
+  "Increases job progress"
+  [{:keys [id value]}]
+  ((keyword (str id))
+   (swap! job-db
+          (fn [all-jobs id value]
+            (let [jobs (unexpired-jobs
+                        all-jobs
+                        job-expire-period-seconds)]
+              (if-let [job ((keyword (str id)) jobs)]
+                (let [new-progress (+ (:progress job) value)
+                      job {:id id
+                           :total (:total job)
+                           :progress (if (<= new-progress (:total job))
+                                       new-progress
+                                       (:total job))
+                           :updated-at (current-unix-timestamp)}]
+                  (assoc jobs (keyword (str id)) job))
+                jobs)) )
+          id
+          value)))
+
+(defn set-progress
+  "Set job progress"
+  [{:keys [id value]}]
+  ((keyword (str id))
+   (swap! job-db
+          (fn [all-jobs id value]
+            (let [jobs (unexpired-jobs
+                        all-jobs
+                        job-expire-period-seconds)]
+              (if-let [job ((keyword (str id)) jobs)]
+                (let [job {:id id
+                           :total (:total job)
+                           :progress (if (<= value (:total job))
+                                       value
+                                       (:total job))
+                           :updated-at (current-unix-timestamp)}]
+                  (assoc jobs (keyword (str id)) job))
+                jobs)) )
+          id
+          value)))
 
 (defroutes job-routes
   (GET "/" []
-       (ok (get-jobs)))
+       (ok (map
+            #(dissoc (val %) :updated-at)
+            (get-jobs))))
+  (POST "/" []
+        :body [body {:id Long}]
+        (ok (dissoc (get-job (:id body)) :updated-at)))
   (POST "/add-job" []
         :body [body {:total Long}]
-        (ok {:id (:id (add-new-job body))})))
+        (ok {:id (:id (add-new-job body))}))
+  (POST "/inc-progress" []
+        :body [body {:id Long
+                     :value Long}]
+        (ok {:progress (:progress (inc-progress body))}))
+  (POST "/set-progress" []
+        :body [body {:id Long
+                     :value Long}]
+        (ok {:progress (:progress (set-progress body))})))
--- test/jpt/job_test.clj
+++ test/jpt/job_test.clj
@@ -7,14 +7,15 @@
 
 (defn clear-job-db-fixture [f]
   (f)
-  (reset! job-db [])
+  (reset! job-db {})
   (reset! job-id 1))
 
 (use-fixtures :each clear-job-db-fixture)
 
 (defn- parse-body
   [body]
-  (json/parse-string (slurp body) true))
+  (if body
+    (json/parse-string (slurp body) true)))
 
 (defn- random-job-progress
   []
@@ -57,3 +58,119 @@
           body (parse-body (:body response))]
       (is (= (:status response) 200))
       (is (= (count body) jobs-created)))))
+
+(deftest test-get-job
+  (testing "Testing POST request to /jobs"
+    (testing "Should return null"
+      (let [response (app (-> (mock/request :post "/jobs")
+                              (mock/content-type "application/json")
+                              (mock/json-body {:id 1})))
+            body (parse-body (:body response))]
+        (is (= (:status response) 200))
+        (is (= body nil))))
+    (testing "Should return job by id"
+      (let [job (add-new-job {:total 100})
+            response (app (-> (mock/request :post "/jobs")
+                              (mock/content-type "application/json")
+                              (mock/json-body {:id (:id job)})))
+            body (parse-body (:body response))]
+        (is (= (:status response) 200))
+        (is (= body {:id (:id job)
+                     :progress 0
+                     :total 100}))))))
+
+(deftest test-inc-progress
+  (testing "Testing POST request to /jobs/inc-progress"
+    (testing "Should return null progress"
+      (let [response (app (-> (mock/request :post "/jobs/inc-progress")
+                              (mock/content-type "application/json")
+                              (mock/json-body {:id 1
+                                               :value 10})))
+            body (parse-body (:body response))]
+        (is (= (:status response) 200))
+        (is (= (:progress body) nil))))
+    (testing "Increase progress"
+      (let [job (add-new-job {:total 100})
+            response (app (-> (mock/request :post "/jobs/inc-progress")
+                              (mock/content-type "application/json")
+                              (mock/json-body {:id (:id job)
+                                               :value 10})))
+            body (parse-body (:body response))]
+        (is (= (:status response) 200))
+        (is (= (:progress body) 10))))
+    (testing "Progress should not be greater than total"
+      (let [job (add-new-job {:total 100})
+            response (app (-> (mock/request :post "/jobs/inc-progress")
+                              (mock/content-type "application/json")
+                              (mock/json-body {:id (:id job)
+                                               :value 200})))
+            body (parse-body (:body response))]
+        (is (= (:status response) 200))
+        (is (= (:progress body) 100))))))
+
+(deftest stress-test-inc-progress
+  (testing "Submit lots of concurent inc progress to single job"
+    (let [job (add-new-job {:total 10000000})
+          inc-count
+          (count
+           (pmap #(app (-> (mock/request :post "/jobs/inc-progress")
+                          (mock/content-type "application/json")
+                          (mock/json-body {:id (:id job)
+                                           :value %})))
+                 (take 1000 (iterate int 1))))
+          response (app (-> (mock/request :post "/jobs")
+                            (mock/content-type "application/json")
+                            (mock/json-body {:id (:id job)})))
+          body (parse-body (:body response))]
+      (is (= (:status response) 200))
+      (is (= body {:id (:id job)
+                   :progress inc-count
+                   :total (:total job)})))))
+
+(deftest test-set-progress
+  (testing "Testing POST request to /jobs/set-progress"
+    (testing "Should return null progress"
+      (let [response (app (-> (mock/request :post "/jobs/set-progress")
+                              (mock/content-type "application/json")
+                              (mock/json-body {:id 1
+                                               :value 10})))
+            body (parse-body (:body response))]
+        (is (= (:status response) 200))
+        (is (= (:progress body) nil))))
+    (testing "Set progress"
+      (let [job (add-new-job {:total 200})
+            response (app (-> (mock/request :post "/jobs/set-progress")
+                              (mock/content-type "application/json")
+                              (mock/json-body {:id (:id job)
+                                               :value 55})))
+            body (parse-body (:body response))]
+        (is (= (:status response) 200))
+        (is (= (:progress body) 55))))
+    (testing "Progress should not be greater than total"
+      (let [job (add-new-job {:total 300})
+            response (app (-> (mock/request :post "/jobs/inc-progress")
+                              (mock/content-type "application/json")
+                              (mock/json-body {:id (:id job)
+                                               :value 700})))
+            body (parse-body (:body response))]
+        (is (= (:status response) 200))
+        (is (= (:progress body) 300))))))
+
+(deftest stress-test-set-progress
+  (testing "Submit lots of concurent set progress to single job"
+    (let [job (add-new-job {:total 510})
+          inc-count
+          (count
+           (pmap #(app (-> (mock/request :post "/jobs/set-progress")
+                           (mock/content-type "application/json")
+                           (mock/json-body {:id (:id job)
+                                            :value %})))
+                 (take 1000 (iterate int 500))))
+          response (app (-> (mock/request :post "/jobs")
+                            (mock/content-type "application/json")
+                            (mock/json-body {:id (:id job)})))
+          body (parse-body (:body response))]
+      (is (= (:status response) 200))
+      (is (= body {:id (:id job)
+                   :progress 500
+                   :total (:total job)})))))
